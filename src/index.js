8735865684a2ebf8b1981e4b10bf1c2889b7c27f
orest-forest <danilchenko.orest@gmail.com>
2020-09-30 12:06:21 +0300

    feat: add max, min and avg funcions for arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
+exports.min = function min(array) {
+    if (array == undefined || Array.isArray(array) && array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let minValue = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i];
+        }
+    }
+    return minValue;
+};
+
+exports.max = function max(array) {
+    if (array == undefined || Array.isArray(array) && array.length === 0) {
+        return 0;
+    }
+
+    if (array.length === 0) {
+        return 0
+    }
+
+    let maxValue = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxValue) {
+            maxValue = array[i];
+        }
+    }
+    return maxValue;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || Array.isArray(array) && array.length === 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    if (array.length === 0) {
+        return 0
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    return sum / array.length
+
 }
